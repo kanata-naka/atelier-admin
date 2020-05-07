18f427cbd9719b68747cdc68915a4729018901e1
kanata-fabiko <faubynet@gmail.com>
2020-05-07 13:07:00 +0900

    環境変数の定義をnext.config.jsに移動

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 const withSass = require("@zeit/next-sass")
+const config = require("config")
 
 module.exports = {
-  ...withSass()
+  ...withSass(),
+  serverRuntimeConfig: {
+    ENVIRONMENT: process.env.NODE_ENV,
+    API_BASE_URL: config.get("api.baseUrl"),
+    FIREBASE_API_KEY: config.get("firebase.apiKey"),
+    FIREBASE_AUTH_DOMAIN: config.get("firebase.authDomain"),
+    FIREBASE_PROJECT_ID: config.get("firebase.projectId"),
+    FIREBASE_STORAGE_BUCKET: config.get("firebase.storageBucket"),
+    FIREBASE_REGION: config.get("firebase.region")
+  }
 }
